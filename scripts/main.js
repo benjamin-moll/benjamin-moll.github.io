713bbe16feabe3d81d9835436b66d516fd79f78d
benjamin-moll <54908849+benjamin-moll@users.noreply.github.com>
2020-07-27 12:30:52 -0400

    adding a greeting message function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/test-meme.png') {
-      myImage.setAttribute ('src','images/ben.jpg');
-    } else {
-      myImage.setAttribute ('src','images/test-meme.jpg');
-    }
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
 }
