b70a2055ae395a394864b06e902652c8a50443e8
benjamin-moll <54908849+benjamin-moll@users.noreply.github.com>
2020-07-27 12:16:33 -0400

    adding a second image on click script

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/test-meme.png') {
+      myImage.setAttribute ('src','images/ben.jpg');
+    } else {
+      myImage.setAttribute ('src','images/ben.jpg');
+    }
+}
